9c1a1bf87ce1d55cd369794107c96debb9efe84e
amsem <aminesaidsel@gmail.com>
2023-02-10 11:19:03 +0100

    Adding the config.go file to setup the MYSQL DB

--- package/config/app.go
+++ package/config/app.go
@@ -1,6 +1,21 @@
 package config
 
-import(
+import (
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
--- package/routes/book-routes.go
+++ package/routes/book-routes.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/amsem/Book-Managment/package/controlers"
+	"github.com/gorilla/mux"
 )
 
-var RegisterBookStoreRoutes = func (router *mux.Router)  {
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controlers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controlers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controlers.GetBookById).Methods("GET")
